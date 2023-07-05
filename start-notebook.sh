878195a8290a6dfad3e3986425b87d84b33d1085
Jens A. Koch <jakoch@web.de>
2023-07-05 19:56:05 +0200

    set chmod=+x on start-notebook.sh

--- start-notebook.sh
+++ start-notebook.sh
@@ -3,9 +3,14 @@
 # copy jupyter notebook config to root user
 cp -rf conf/.jupyter /root
 
+# copy zsh config to root user
+cp -rf .devcontainer/.zshrc /root
+
 # adding "/root/.local/bin" for pylint (isort, epylint, pylint, etc.)
 export PATH="/root/.local/bin:$PATH"
 
 export DISPLAY=:0
+
+jupyter kernelspec list
 
 jupyter notebook "$@" &
